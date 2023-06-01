f8e573ca2cb2be85779d4d924cc0554e895149e1
S.N Vishwas Kancharlapalli <s.nvishwaskancharlapalli@SNs-iMac.local>
2023-06-01 12:32:01 +0100

    Completed Task 1, with tests for both functions

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,7 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote["stock"], quote["top_bid"]["price"], quote["top_ask"]["price"], (quote["top_bid"]["price"] + quote["top_ask"]["price"])/2 ))
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
       {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
@@ -16,9 +17,29 @@ class ClientTest(unittest.TestCase):
     ]
     """ ------------ Add the assertion below ------------ """
 
+    """ 
+        Note: the assertion here is the same as the assertion in the test above.
+        This is because, as the test name indicates, it is the dummy test data that is different.
+        The top_bid_price is always higher than the top_ask_price in the quotes dummy dictionary above.
+    """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote["stock"], quote["top_bid"]["price"], quote["top_ask"]["price"], (quote["top_bid"]["price"] + quote["top_ask"]["price"])/2 ))
 
   """ ------------ Add more unit tests ------------ """
 
+  def test_getRatio_calculateRatioWhenPriceAIsZero(self):
+    mock_price_a = 0
+    """ Random price b as it is not relevant to the test case """
+    mock_price_b = 300
+
+    self.assertEqual(getRatio(mock_price_a, mock_price_b), 0)
+
+  def test_getRatio_calculateRatioWhenPricePriceBIsZero(self):
+    """ Random price a as it is not relevant to the test case """
+    mock_price_a = 500
+    mock_price_b = 0
+
+    self.assertEqual(getRatio(mock_price_a, mock_price_b), None)
 
 
 if __name__ == '__main__':
